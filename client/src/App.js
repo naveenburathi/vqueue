f12b0634416d37531ec537ebf55f1890e363b073
Naveen Burathi <naveenburathi1@gamil.comgit config --global user.name Naveen>
2022-06-29 20:00:36 +0530

    client added createQueue and queue pages

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Home from "./pages/Home";
 import theme from "./assets/theme";
 import Navbar from "./components/Navbar";
 import Auth from "./pages/Auth";
+import Queue from "./pages/Queue";
+import CreateQueue from "./pages/CreateQueue";
 
 const App = () => {
   return (
@@ -20,6 +22,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/queue" element={<Queue />} />
+          <Route path="/createqueue" element={<CreateQueue />} />
         </Routes>
       </Router>
     </ThemeProvider>
